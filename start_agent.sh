b386290b1d91ea4c3b4cd56ce658c8850498b7d2
sean tearney <sean@ispyconnect.com>
2021-12-16 18:14:17 +0800

    set library path

--- start_agent.sh
+++ start_agent.sh
@@ -3,7 +3,10 @@
 # To execute: save and `chmod +x ./start_agent.sh` then `./start_agent.sh`
 
 ABSOLUTE_PATH="${PWD}"
-export PATH="$PATH:$ABSOLUTE_PATH/.dotnet:$ABSOLUTE_PATH/AgentDVR/ffmpeg-build/workspace/lib"
+export PATH=$PATH:$ABSOLUTE_PATH/AgentDVR/.dotnet
+
+LD_LIBRARY_PATH="$ABSOLUTE_PATH/AgentDVR/ffmpeg-build/workspace/lib"
+export LD_LIBRARY_PATH
 
 dotnet AgentDVR/Agent.dll
 
