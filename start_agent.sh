4c1576802cd180c25b09835b6f0e267f92854f88
sean tearney <sean@ispyconnect.com>
2021-12-16 17:53:53 +0800

    fix paths

--- start_agent.sh
+++ start_agent.sh
@@ -3,7 +3,7 @@
 # To execute: save and `chmod +x ./start_agent.sh` then `./start_agent.sh`
 
 ABSOLUTE_PATH="${PWD}"
-export PATH="$PATH:$ABSOLUTE_PATH/.dotnet:$ABSOLUTE_PATH/ffmpeg-build/workspace/lib"
+export PATH="$PATH:$ABSOLUTE_PATH/.dotnet:$ABSOLUTE_PATH/AgentDVR/ffmpeg-build/workspace/lib"
 
-dotnet Agent.dll
+dotnet AgentDVR/Agent.dll
 
