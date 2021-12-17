3608bb0825131cdff56341286e21155efa0b342a
sean tearney <sean@ispyconnect.com>
2021-12-17 12:34:03 +0800

    Update macos_setup.sh

--- macos_setup.sh
+++ macos_setup.sh
@@ -80,11 +80,11 @@ then
 		read -p "Enter your username [$(whoami)]: " name
 		name=${name:-$(whoami)}
 		curl --show-error --location "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/com.ispy.agent.dvr.plist" -o "com.ispy.agent.dvr.plist"
-		sed -i '' 's|AGENT_LOCATION|$ABSOLUTE_PATH/AgentDVR|' com.ispy.agent.dvr.plist
-		sed -i '' 's|YOUR_USERNAME|$name|' com.ispy.agent.dvr.plist
+		sed -i '' 's|AGENT_LOCATION|$(ABSOLUTE_PATH)/AgentDVR|' com.ispy.agent.dvr.plist
+		sed -i '' 's|YOUR_USERNAME|$(name)|' com.ispy.agent.dvr.plist
 		sudo chmod a+x ./com.ispy.agent.dvr.plist
 		sudo chown root:wheel ./com.ispy.agent.dvr.plist
-		sudo chown $(name) -R $ABSOLUTE_PATH/AgentDVR
+		sudo chown $(name) -R $(ABSOLUTE_PATH)/AgentDVR
 		sudo cp com.ispy.agent.dvr.plist /Library/LaunchDaemons/
 		sudo launchctl load -w /Library/LaunchDaemons/com.ispy.agent.dvr.plist
 		echo "Started service"
