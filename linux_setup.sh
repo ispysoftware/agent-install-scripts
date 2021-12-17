f55d52107cebe392a822a42e2294c52cda0e396b
sean tearney <sean@ispyconnect.com>
2021-12-17 08:58:54 +0800

    bug fixes, add prompts

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# Homebrew Script for AgentDVR/ Linux
+# Install script for AgentDVR/ Linux
 # To execute: save and `chmod +x ./agent_setup.sh` then `./agent_setup.sh`
 
 ABSOLUTE_PATH="${PWD}"
-
 machine_has() {
     eval $invocation
 
@@ -14,10 +13,10 @@ machine_has() {
 
 if machine_has "apt-get"; then
 	sudo apt-get update \
-		&& apt-get install -y unzip python3 curl make g++ build-essential
+		&& sudo apt-get install -y unzip python3 curl make g++ build-essential
 else
 	sudo yum update \
-		&& yum install -y autoconf automake bzip2 bzip2-devel cmake freetype-devel gcc gcc-c++ git libtool make pkgconfig zlib-devel
+		&& sudo yum install -y autoconf automake bzip2 bzip2-devel cmake freetype-devel gcc gcc-c++ git libtool make pkgconfig zlib-devel
 fi
 
 
@@ -35,56 +34,81 @@ cd AgentDVR
 FILE=$ABSOLUTE_PATH/AgentDVR/Agent.dll
 if [ ! -f $FILE ]
 then
+	echo "finding installer for $(arch)"
 	purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=true&platform=Linux"
+	case $(arch) in
+		'aarch64' | 'arm64')
+			purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=true&platform=ARM"
+		;;
+		'arm' | 'armv6l' | 'armv7l')
+			purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=false&platform=ARM32"
+		;;
+	esac
 
-	if [ $arch = 'aarch64' ] || [ $arch = 'arm64' ]
-	then
-		purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=true&platform=ARM"
-	fi
-	if [ $arch = 'arm' ] || [ $arch = 'armv7l' || [ $arch = 'armv6l' ]
-	then
-		purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=false&platform=ARM32"
-	fi
 	AGENTURL=$(curl -s --fail "$purl" | tr -d '"')
 	echo "Downloading $AGENTURL"
 	curl --show-error --location "$AGENTURL" -o "AgentDVR.zip"
 	unzip AgentDVR.zip && \ 
 	rm AgentDVR.zip
+else
+	echo "Found Agent in $ABSOLUTE_PATH/AgentDVR - delete it to reinstall"
 fi
 
-if [ ! -d $ABSOLUTE_PATH/.dotnet ]
+if [ ! -d $ABSOLUTE_PATH/AgentDVR/.dotnet ]
 then
-	echo "Installing dotnet"
-	curl -s "https://dot.net/v1/dotnet-install.sh" | bash -s -- --version "3.1.300" --install-dir "$ABSOLUTE_PATH/.dotnet"
+	echo -n "Install dotnet 3.1.300 for Agent (y/n)? "
+	read answer
+	if [ "$answer" != "${answer#[Yy]}" ] ;then 
+		echo Yes
+		echo "Installing dotnet"
+		curl -s "https://dot.net/v1/dotnet-install.sh" | bash -s -- --version "3.1.300" --install-dir "$ABSOLUTE_PATH/AgentDVR/.dotnet"
+	fi
+else
+	echo "Found dotnet in $ABSOLUTE_PATH/AgentDVR/.dotnet - delete it to reinstall"
 fi
 
 if [ ! -d $ABSOLUTE_PATH/AgentDVR/ffmpeg-build ]
 then
-	mkdir ffmpeg-build
-	cd ffmpeg-build
-	curl -s "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/ffmpeg_build.sh" | bash -s -- --build --enable-gpl-and-non-free
+	echo -n "Build ffmpeg for Agent (y/n)? "
+	read answer
+	if [ "$answer" != "${answer#[Yy]}" ] ;then 
+		echo Yes
+		mkdir ffmpeg-build
+		cd ffmpeg-build
+		curl -s "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/ffmpeg_build.sh" | bash -s -- --build --enable-gpl-and-non-free
+	fi
+else
+	echo "Found ffmpeg in $ABSOLUTE_PATH/AgentDVR/ffmpeg-build - delete it to reinstall"
 fi
 
 cd $ABSOLUTE_PATH
 
-echo -n "Install AgentDVR as system service (y/n)? "
-read answer
-if [ "$answer" != "${answer#[Yy]}" ] ;then 
-	echo Yes
-	read -p "Enter your username [$(whoami)]: " name
-	name=${name:-$(whoami)}
-	curl --show-error --location "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/AgentDVR.service" -o "AgentDVR.service"
-	sed -i "s|AGENT_LOCATION|$ABSOLUTE_PATH|" AgentDVR.service
-	sed -i "s|YOUR_USERNAME|$name|" AgentDVR.service
-	sudo chmod a+x ./AgentDVR.service
-	sudo chown $name -R $ABSOLUTE_PATH/AgentDVR
-	sudo cp AgentDVR.service /etc/systemd/system/AgentDVR.service
-	sudo systemctl daemon-reload
-	sudo systemctl start AgentDVR
-	echo "started service"
-	echo "go to http://localhost:8090 to configure"
+
+FILE=/etc/systemd/system/AgentDVR.service
+if [ ! -f $FILE ]
+then
+	echo -n "Install AgentDVR as system service (y/n)? "
+	read answer
+	if [ "$answer" != "${answer#[Yy]}" ] ;then 
+		echo Yes
+		read -p "Enter your username [$(whoami)]: " name
+		name=${name:-$(whoami)}
+		curl --show-error --location "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/AgentDVR.service" -o "AgentDVR.service"
+		sed -i "s|AGENT_LOCATION|$ABSOLUTE_PATH|" AgentDVR.service
+		sed -i "s|YOUR_USERNAME|$name|" AgentDVR.service
+		sudo chmod a+x ./AgentDVR.service
+		sudo chown $name -R $ABSOLUTE_PATH/AgentDVR
+		sudo cp AgentDVR.service /etc/systemd/system/AgentDVR.service
+		sudo systemctl daemon-reload
+		sudo systemctl start AgentDVR
+		echo "started service"
+		echo "go to http://localhost:8090 to configure"
+		exit
+	else
+		./start_agent.sh
+	fi
 else
-	./start_agent.sh
+	echo "Found service definition in /etc/systemd/system/AgentDVR.service - delete it to disable"
 fi
 
 exit
