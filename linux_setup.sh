43ccb6f127bb61a806606f09dcac5d940f2e3bb2
sean tearney <sean@ispyconnect.com>
2021-12-21 10:04:29 +0800

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -3,6 +3,9 @@
 # Install script for AgentDVR/ Linux
 # To execute: save and `chmod +x ./agent_setup.sh` then `./agent_setup.sh`
 
+. /etc/lsb-release
+
+
 if [[ ("$OSTYPE" == "darwin"*) ]]; then
   # If arm64 AND darwin (macOS)
   echo "Use use macos_setup.sh instead"
@@ -25,6 +28,7 @@ else
 	sudo yum update \
 		&& sudo yum install -y autoconf automake bzip2 bzip2-devel cmake freetype-devel gcc gcc-c++ git libtool make pkgconfig zlib-devel libvlc-dev vlc libx11-dev
 fi
+
 
 
 FILE=$ABSOLUTE_PATH/start_agent.sh
@@ -73,7 +77,29 @@ else
 	echo "Found dotnet in $ABSOLUTE_PATH/AgentDVR/.dotnet - delete it to reinstall"
 fi
 
-if [ ! -d $ABSOLUTE_PATH/AgentDVR/ffmpeg-build ]
+ffmpeg_installed=false
+
+if [ $DISTRIB_ID == "Ubuntu" ] ;then
+	if (( $(echo "$DISTRIB_RELEASE > 21" | bc -l) )); then
+		echo "Installing ffmpeg from default package manager (Ubuntu 21+)"
+		sudo apt install ffmpeg
+		# ubuntu 20+ needs a symlink to libdl to run properly
+		if [ ! -f /lib/x86_64-linux-gnu/libdl.so ]; then
+			sudo ln -s /lib/x86_64-linux-gnu/libdl.so.2 /lib/x86_64-linux-gnu/libdl.so
+		fi
+	else
+		echo "Installing ffmpeg from jonathonf repo"
+		sudo apt-get update
+		sudo add-apt-repository ppa:jonathonf/ffmpeg-4
+		sudo apt-get update
+		sudo apt-get install -y ffmpeg
+	fi
+	ffmpeg_installed=true
+else
+	echo "No default ffmpeg package option - build from source"
+fi
+
+if [ "$ffmpeg_installed" = false ] && ! [ -d $ABSOLUTE_PATH/AgentDVR/ffmpeg-build ]
 then
 	read -p "Build ffmpeg for Agent (y/n)? " answer
 	if [ "$answer" != "${answer#[Yy]}" ] ;then 
@@ -83,7 +109,7 @@ then
 		curl -s -L "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/ffmpeg_build.sh" | bash -s -- --build --enable-gpl-and-non-free
 	fi
 else
-	echo "Found ffmpeg in $ABSOLUTE_PATH/AgentDVR/ffmpeg-build - delete it to reinstall"
+	echo "Found ffmpeg"
 fi
 
 cd $ABSOLUTE_PATH
