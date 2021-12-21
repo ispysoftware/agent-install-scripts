2b87f739d945f5bd06798eeb03719f451eb365ff
sean tearney <sean@ispyconnect.com>
2021-12-21 09:07:52 +0800

    Update linux_test.sh

--- linux_test.sh
+++ linux_test.sh
@@ -3,6 +3,9 @@
 # Install script for AgentDVR/ Linux
 # To execute: save and `chmod +x ./agent_setup.sh` then `./agent_setup.sh`
 
+. /etc/lsb-release
+
+
 if [[ ("$OSTYPE" == "darwin"*) ]]; then
   # If arm64 AND darwin (macOS)
   echo "Use use macos_setup.sh instead"
@@ -25,6 +28,7 @@ else
 	sudo yum update \
 		&& sudo yum install -y autoconf automake bzip2 bzip2-devel cmake freetype-devel gcc gcc-c++ git libtool make pkgconfig zlib-devel libvlc-dev vlc libx11-dev
 fi
+
 
 
 FILE=$ABSOLUTE_PATH/start_agent.sh
@@ -73,7 +77,22 @@ else
 	echo "Found dotnet in $ABSOLUTE_PATH/AgentDVR/.dotnet - delete it to reinstall"
 fi
 
-if [ ! -d $ABSOLUTE_PATH/AgentDVR/ffmpeg-build ]
+ffmpeg_installed = false
+if [ $DISTRIB_ID == "Ubuntu" ] ;then
+	if [ $DISTRIB_RELEASE -gt "20" ] ;then
+		echo "Installing ffmpeg from default package manager (Ubuntu 20+)"
+		sudo apt install ffmpeg
+	else
+		echo "Installing ffmpeg from jonathonf repo"
+		sudo apt-get update
+		sudo add-apt-repository ppa:jonathonf/ffmpeg-4
+		sudo apt-get update
+		sudo apt-get install -y ffmpeg
+	fi
+	ffmpeg_installed = true
+fi
+
+if [ "$ffmpeg_installed" = false ] && ! [ -d $ABSOLUTE_PATH/AgentDVR/ffmpeg-build ]
 then
 	read -p "Build ffmpeg for Agent (y/n)? " answer
 	if [ "$answer" != "${answer#[Yy]}" ] ;then 
