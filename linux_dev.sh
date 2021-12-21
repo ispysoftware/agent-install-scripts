a719e044e7676a28e3c8f7a2ddb7a8cc5858a9d2
sean tearney <sean@ispyconnect.com>
2021-12-21 11:00:54 +0800

    Update linux_dev.sh

--- linux_dev.sh
+++ linux_dev.sh
@@ -91,7 +91,7 @@ if [ $DISTRIB_ID == "Ubuntu" ] ;then
 		sudo apt-get install -y ffmpeg
 	fi
 	ffmpeg_installed=true
-elif cat /etc/*release | grep ^NAME | grep Debian 
+elif cat /etc/*release | grep ^NAME | grep Debian ; then
 	sudo wget http://security.ubuntu.com/ubuntu/pool/main/libj/libjpeg-turbo/libjpeg-turbo8_1.5.2-0ubuntu5.18.04.4_amd64.deb
 	sudo wget http://fr.archive.ubuntu.com/ubuntu/pool/main/libj/libjpeg8-empty/libjpeg8_8c-2ubuntu8_amd64.deb
 	sudo apt install multiarch-support
