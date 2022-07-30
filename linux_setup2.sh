4ad5c88645bff4d6d0be67f5cb295a0c3bc634df
sean tearney <sean@ispyconnect.com>
2022-07-30 10:01:55 +0800

    Create linux_setup2.sh

---
+++ linux_setup2.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+# Install script for AgentDVR/ Linux
+# To execute: save and `chmod +x ./linux_setup2.sh` then `./linux_setup2.sh`
+
+. /etc/lsb-release
+arch=`uname -m`
+
+ffmpeg_installed=false
+
+if [[ ("$OSTYPE" == "darwin"*) ]]; then
+  # If arm64 AND darwin (macOS)
+  echo "Use use macos_setup.sh instead"
+  exit
+fi
+
+
+ABSOLUTE_PATH="${PWD}"
+machine_has() {
+    eval $invocation
+
+    command -v "$1" > /dev/null 2>&1
+    return $?
+}
+ 
+mkdir AgentDVR
+cd AgentDVR
+
+#install ffmpeg 5 
+
+
+#download latest version
+
+FILE=$ABSOLUTE_PATH/AgentDVR/Agent
+if [ ! -f $FILE ]
+then
+	echo "finding installer for $(arch)"
+	purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=true&platform=Linux"
+	case $(arch) in
+		'aarch64' | 'arm64')
+			purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=true&platform=ARM"
+		;;
+		'arm' | 'armv6l' | 'armv7l')
+			purl="https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=false&platform=ARM32"
+		;;
+	esac
+
+	AGENTURL=$(curl -s --fail "$purl" | tr -d '"')
+	echo "Downloading $AGENTURL"
+	curl --show-error --location "$AGENTURL" -o "AgentDVR.zip"
+	unzip AgentDVR.zip
+	rm AgentDVR.zip
+else
+	echo "Found Agent in $ABSOLUTE_PATH/AgentDVR - delete it to reinstall"
+fi
+
+if [ "$DISTRIB_ID" = "Ubuntu" ] ; then
+	sudo add-apt-repository ppa:savoury1/ffmpeg4 -y
+  sudo add-apt-repository ppa:savoury1/ffmpeg5 -y
+  sudo apt update
+  sudo apt upgrade -y
+  sudo apt install ffmpeg -y
+  ffmpeg_installed=true
+else
+	echo "No default ffmpeg package option - build from source"
+fi
+
+if [ "$ffmpeg_installed" = false ]
+then
+	read -p "Build ffmpeg v5 for Agent (y/n)? " answer
+	if [ "$answer" != "${answer#[Yy]}" ] ;then 
+		echo Yes
+		mkdir ffmpeg-build
+		cd ffmpeg-build
+		curl -s -L "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/ffmpeg_build_v5.sh" | bash -s -- --build --enable-gpl-and-non-free
+	fi
+else
+	echo "Found ffmpeg"
+fi
+
+cd $ABSOLUTE_PATH
+
+name=$(whoami)
+#add permissions for local device access
+echo "Adding permission for local device access"
+sudo adduser $name video
+sudo usermod -a -G video $name
+
+echo "Adding execute permissions"
+sudo chmod +x ./Agent
+sudo chmod +x ./CoreUpdater
+
+FILE=/etc/systemd/system/AgentDVR.service
+if [ ! -f $FILE ]
+then
+	
+
+	read -p "Install AgentDVR as system service (y/n)? " answer
+	if [ "$answer" != "${answer#[Yy]}" ] ;then 
+		echo Yes
+		echo "Installing service as $name"
+		curl --show-error --location "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/AgentDVR.service" -o "AgentDVR.service"
+		sed -i "s|AGENT_LOCATION|$ABSOLUTE_PATH|" AgentDVR.service
+		sed -i "s|YOUR_USERNAME|$name|" AgentDVR.service
+		sudo chmod 644 ./AgentDVR.service
+		sudo chown $name -R $ABSOLUTE_PATH/AgentDVR
+		sudo cp AgentDVR.service /etc/systemd/system/AgentDVR.service
+		
+		sudo systemctl daemon-reload
+		sudo systemctl enable AgentDVR.service
+		sudo systemctl start AgentDVR
+		
+		echo "started service"
+		echo "go to http://localhost:8090 to configure"
+		exit 0
+	else
+		./start_agent.sh
+	fi
+else
+	echo "Found service definition in /etc/systemd/system/AgentDVR.service"
+	echo "Go to http://localhost:8090"
+fi
+
+exit 0
