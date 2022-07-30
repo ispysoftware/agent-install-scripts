b620d42d6c51b438592ead640c3ce4a43e1f0339
sean tearney <sean@ispyconnect.com>
2022-07-30 10:21:54 +0800

    Create install.sh

---
+++ v2/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Install script for AgentDVR/ Linux
+# To execute: save and `chmod +x ./install.sh` then `./install.sh`
+
+if [[ ("$OSTYPE" == "darwin"*) ]]; then
+  # If darwin (macOS)
+  bash <(curl -s "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/v2/macos_setup.sh")
+  exit
+fi
+
+bash <(curl -s "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/v2/linux_setup.sh")
