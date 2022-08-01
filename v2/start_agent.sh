b78713b16538a615dad0c0b6195a07c1fb72a4c7
sean tearney <sean@ispyconnect.com>
2022-08-01 19:14:47 +0800

    Update start_agent.sh

--- v2/start_agent.sh
+++ v2/start_agent.sh
@@ -2,7 +2,7 @@
 
 # To execute: save and `chmod +x ./start_agent.sh` then `./start_agent.sh`
 
-LD_LIBRARY_PATH="$ABSOLUTE_PATH/AgentDVR/ffmpeg-build/workspace/lib"
+LD_LIBRARY_PATH="$ABSOLUTE_PATH/ffmpeg-build/workspace/lib"
 export LD_LIBRARY_PATH
 
 ./Agent
