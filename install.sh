9addf0475a644758b52cb0fafec14b0f59978db7
sean tearney <sean@ispyconnect.com>
2022-08-24 09:57:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Install script for AgentDVR/ Linux
 # To execute: save and `chmod +x ./install.sh` then `./install.sh`
 
-#echo "Warning, this install script is capped at v4.1.1.0 due to the project upgraded to .net 6"
+echo "Warning, this install script is capped at v4.1.1.0 due to the project upgraded to .net 6. See the download page at https://www.ispyconnect.com/download.aspx for instructions"
 
 if [[ ("$OSTYPE" == "darwin"*) ]]; then
   # If darwin (macOS)
