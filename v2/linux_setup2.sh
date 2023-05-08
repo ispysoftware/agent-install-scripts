e64adc18d5941d84cffe8fd31a00a2dc897d0b07
sean tearney <sean@ispyconnect.com>
2023-05-08 14:21:49 +0800

    Create linux_setup2.sh

---
+++ v2/linux_setup2.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+# Install script for AgentDVR/ Linux
+# To execute: save and `chmod +x ./linux_setup2.sh` then `./linux_setup2.sh`
+
+. /etc/*-release
+arch=`uname -m`
+
+ffmpeg_installed=false
+
+if [[ ("$OSTYPE" == "darwin"*) ]]; then
+  # If arm64 AND darwin (macOS)
+  echo "Use use osx_setup2.sh instead"
+  exit
+fi
+
+machine_has() {
+    eval $invocation
+
+    command -v "$1" > /dev/null 2>&1
+    return $?
+}
+
+ABSOLUTE_PATH="${PWD}"
+mkdir AgentDVR
+cd AgentDVR
+
+cd $ABSOLUTE_PATH/AgentDVR/
+#download latest version
+
+FILE=$ABSOLUTE_PATH/AgentDVR/Agent
+if [ ! -f $FILE ]
+then
+	echo "Finding installer for $(arch)"
+	purl="https://www.ispyconnect.com/api/Agent/DownloadLocation4?platform=Linux64&fromVersion=0"
+	
+	case $(arch) in
+		'aarch64' | 'arm64')
+			purl="https://www.ispyconnect.com/api/Agent/DownloadLocation4?platform=LinuxARM64&fromVersion=0"
+		;;
+		'arm' | 'armv6l' | 'armv7l')
+      echo "32 bit not supported - install a 64 bit OS"
+      exit
+		;;
+	esac
+
+	AGENTURL=$(curl -s --fail "$purl" | tr -d '"')
+	echo "Downloading $AGENTURL"
+	curl --show-error --location "$AGENTURL" -o "AgentDVR.zip"
+	
+	if machine_has "apt-get"; then
+		sudo apt-get unzip
+	else
+		sudo yum install bzip2
+	fi
+	
+	unzip AgentDVR.zip
+	rm AgentDVR.zip
+else
+	echo "Found Agent in $ABSOLUTE_PATH/AgentDVR - delete it to reinstall"
+fi
+
+#for backward compat with existing service files
+
+echo "Downloading start script for back compat"
+curl --show-error --location "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/v2/start_agent.sh" -o "start_agent.sh"
+chmod a+x ./start_agent.sh
+
+echo "Adding execute permissions"
+chmod +x ./Agent
+find . -name "*.sh" -exec chmod +x {} \;
+
+cd $ABSOLUTE_PATH
+
+name=$(whoami)
+#add permissions for local device access
+echo "Adding permission for local device access"
+sudo adduser $name video
+sudo usermod -a -G video $name
+
+echo "To run Agent either call ./Agent from the terminal or install it as a system service."
+
+read -p "Setup AgentDVR as system service (y/n)? " answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then 
+	echo Yes
+	echo "Installing service as $name"
+	curl --show-error --location "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/v2/AgentDVR.service" -o "AgentDVR.service"
+	sed -i "s|AGENT_LOCATION|$ABSOLUTE_PATH/AgentDVR|" AgentDVR.service
+	sed -i "s|YOUR_USERNAME|$name|" AgentDVR.service
+	sudo chmod 644 ./AgentDVR.service
+	
+	sudo systemctl stop AgentDVR.service
+  	sudo systemctl disable AgentDVR.service
+	if [ -f /etc/systemd/system/AgentDVR.service ]; then
+  		sudo rm /etc/systemd/system/AgentDVR.service
+	fi
+  
+	sudo chown $name -R $ABSOLUTE_PATH/AgentDVR
+	sudo cp AgentDVR.service /etc/systemd/system/AgentDVR.service
+
+	sudo systemctl daemon-reload
+	sudo systemctl enable AgentDVR.service
+	sudo systemctl start AgentDVR
+
+	echo "Started service"
+	echo "Go to http://localhost:8090 to configure"
+	exit 0
+else
+	echo "Starting Agent DVR from the terminal.."
+	cd AgentDVR
+	./Agent
+fi
+
+exit 0
