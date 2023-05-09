237b35155e1ade2ac7a149de6def376a2a48a368
sean tearney <sean@ispyconnect.com>
2023-05-09 19:14:02 +0800

    Update install.sh

--- v2/install.sh
+++ v2/install.sh
@@ -5,8 +5,8 @@
 
 if [[ ("$OSTYPE" == "darwin"*) ]]; then
   # If darwin (macOS)
-  bash <(curl -H 'Cache-Control: no-cache, no-store' -s "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/v2/osx_setup2.sh")
+  bash <(curl -H 'Cache-Control: no-cache, no-store' -s "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/v2/osx_setup.sh")
   exit
 fi
 
-bash <(curl -H 'Cache-Control: no-cache, no-store' -s "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/v2/linux_setup2.sh")
+bash <(curl -H 'Cache-Control: no-cache, no-store' -s "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/v2/linux_setup.sh")
