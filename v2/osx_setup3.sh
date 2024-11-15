d5b27a7356d19cc67d45a2eb11ab29e471cf2078
sean t <sean@ispyconnect.com>
2024-11-15 16:43:25 +0800

    Update osx_setup3.sh

--- v2/osx_setup3.sh
+++ v2/osx_setup3.sh
@@ -6,25 +6,26 @@ shopt -s expand_aliases
 
 arch=`uname -m`
 
-ABSOLUTE_PATH="${PWD}"
+ABSOLUTE_PATH="$HOME/Applications"
 PLIST_NAME="com.ispy.agent.dvr.plist"
 SYSTEM_LAUNCHDAEMONS_DIR="/Library/LaunchDaemons"
 USER_LAUNCHAGENTS_DIR="$HOME/Library/LaunchAgents"
 
-mkdir -p AgentDVR
-cd AgentDVR
+# Create the Applications directory if it doesn't exist
+mkdir -p "$ABSOLUTE_PATH/AgentDVR"
+cd "$ABSOLUTE_PATH/AgentDVR"
 
 FILE=/usr/local/bin/brew
 if [[ ("$(arch)" == "arm64") ]]; then
-	FILE=/opt/homebrew/bin/brew
+    FILE=/opt/homebrew/bin/brew
 fi
 if [ -f $FILE ]; then
-	echo "Using installed homebrew at $FILE"
-	alias brewcmd='$FILE'
+    echo "Using installed homebrew at $FILE"
+    alias brewcmd='$FILE'
 else
-	echo "Installing homebrew"
-	bash <(curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh")
-	alias brewcmd='$FILE'
+    echo "Installing homebrew"
+    bash <(curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh")
+    alias brewcmd='$FILE'
 fi
 
 formula_installed() {
@@ -34,30 +35,29 @@ formula_installed() {
 
 formula_installed ffmpeg 7
 if [ $? -eq 1 ]; then
-	echo "Installing ffmpeg"
-	brewcmd install curl ca-certificates
-	brewcmd install ffmpeg@7
+    echo "Installing ffmpeg"
+    brewcmd install curl ca-certificates
+    brewcmd install ffmpeg@7
 else
-	echo "Found FFmpeg v7"
+    echo "Found FFmpeg v7"
 fi
-
 
 FILE=$ABSOLUTE_PATH/AgentDVR/Agent
 if [ -f $FILE ]; then
-	echo "Found Agent in $ABSOLUTE_PATH/AgentDVR - delete it to reinstall"
+    echo "Found Agent in $ABSOLUTE_PATH/AgentDVR - delete it to reinstall"
 else
-  echo "Installing to $ABSOLUTE_PATH/AgentDVR"
+    echo "Installing to $ABSOLUTE_PATH/AgentDVR"
 
-  if [[ "$arch" == "arm64" ]]; then
-      URL=$((curl -s -L "https://www.ispyconnect.com/api/Agent/DownloadLocation4?platform=OSXARM64&fromVersion=0") | tr -d '"')
-  else
-      URL=$((curl -s -L "https://www.ispyconnect.com/api/Agent/DownloadLocation4?platform=OSX64&fromVersion=0") | tr -d '"')
-  fi
-  URL="https://ispyrtcdata.blob.core.windows.net/downloads/Agent_OSXARM64_5_8_1_0.zip"
-  echo "Downloading $URL"
-  curl --show-error --location $URL | tar -xf - -C "$ABSOLUTE_PATH/AgentDVR"
-  chmod +x Agent
-  find . -name "*.sh" -exec chmod +x {} \;
+    if [[ "$arch" == "arm64" ]]; then
+        URL=$((curl -s -L "https://www.ispyconnect.com/api/Agent/DownloadLocation4?platform=OSXARM64&fromVersion=0") | tr -d '"')
+    else
+        URL=$((curl -s -L "https://www.ispyconnect.com/api/Agent/DownloadLocation4?platform=OSX64&fromVersion=0") | tr -d '"')
+    fi
+    URL="https://ispyrtcdata.blob.core.windows.net/downloads/Agent_OSXARM64_5_8_1_0.zip"
+    echo "Downloading $URL"
+    curl --show-error --location $URL | tar -xf - -C "$ABSOLUTE_PATH/AgentDVR"
+    chmod +x Agent
+    find . -name "*.sh" -exec chmod +x {} \;
 fi
 
 echo -n "Setup AgentDVR as launch agent (y/n)? "
@@ -78,30 +78,30 @@ if [ "$answer" != "${answer#[Yy]}" ]; then
 
     # Unload any existing daemon
     if [ -f "$SYSTEM_LAUNCHDAEMONS_DIR/$PLIST_NAME" ]; then
-            sudo launchctl unload -w "$SYSTEM_LAUNCHDAEMONS_DIR/$PLIST_NAME"
-            sudo rm -f "$SYSTEM_LAUNCHDAEMONS_DIR/$PLIST_NAME"
+        sudo launchctl unload -w "$SYSTEM_LAUNCHDAEMONS_DIR/$PLIST_NAME"
+        sudo rm -f "$SYSTEM_LAUNCHDAEMONS_DIR/$PLIST_NAME"
     fi
-        
+
     # Unload any existing agent
     if [ -f "$USER_LAUNCHAGENTS_DIR/$PLIST_NAME" ]; then
         launchctl unload -w "$USER_LAUNCHAGENTS_DIR/$PLIST_NAME"
         rm -f "$USER_LAUNCHAGENTS_DIR/$PLIST_NAME"
     fi
-        
+
     # Move plist to LaunchAgents and set permissions
     cp "$PLIST_NAME" "$USER_LAUNCHAGENTS_DIR/"
     rm -f "$PLIST_NAME"
     chmod 644 "$USER_LAUNCHAGENTS_DIR/$PLIST_NAME"
     chown "$(id -u):$(id -g)" "$USER_LAUNCHAGENTS_DIR/$PLIST_NAME"
-        
+
     # Load the new agent
     launchctl load -w "$USER_LAUNCHAGENTS_DIR/$PLIST_NAME"
-        
+
     echo "Launch Agent started successfully."
     echo "Visit http://localhost:8090 to configure"
     exit
 else
     echo "Starting AgentDVR"
-    cd "$ABSOLUTE_PATH"
+    cd "$ABSOLUTE_PATH/AgentDVR"
     ./Agent
 fi
